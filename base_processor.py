132f6c1f3bc15d2801312c8da016354be6c8cccc
syedwahajali2 <wahaj9167@gmail.com>
2026-07-16 07:38:11 +0000

    Add base_processor.py

---
+++ base_processor.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from typing import Any
+class processor:
+    """  today i create this project
+    """
+    def __init__(self, name, version):
+        self._name = name
+        self._version = version = "1.00"
+    @property
+    def name(self):
+        return self._name
+    @property
+    def version(self):
+        return self._version
+    
+    @abstractmethod
+    def process(self):
+        """ hi my name is wahaj"""
+        pass
+    
+    @abstractmethod
+    def summary (self):
+        """ hello"""
+        pass
+    def __repr__(self):
+        return f"{self.__class__.__name__} name ={self._name} version = {self._version}"
+        
